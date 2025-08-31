eebd7eea0a2292297607f8c0545ebf829259820d
Piotr Majkrzak <piotr@majkrzak.dev>
2025-08-31 13:17:15 +0200

    Add script files

--- flake.nix
+++ flake.nix
@@ -1,3 +1,50 @@
 {
-  outputs = { self }: { };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixfmt;
+        packages = rec {
+          default = pkgs.resholve.writeScriptBin "my-script" {
+            inputs = [
+              pkgs.coreutils
+              pkgs.jsonschema
+              pkgs.yq-go
+            ];
+            fix = {
+              "$KRM_SCHEMA" = [
+                (pkgs.writeTextFile {
+                  name = "krm.schema.json";
+                  text = builtins.readFile ./krm.schema.json;
+                })
+              ];
+            };
+            execer = [
+              "cannot:${pkgs.yq-go}/bin/yq"
+              "cannot:${pkgs.jsonschema}/bin/jv"
+            ];
+            interpreter = "${pkgs.runtimeShell}";
+          } (builtins.readFile ./script.sh);
+          docker = pkgs.dockerTools.buildImage {
+            name = "playground";
+            config = {
+              Cmd = [ "${default}" ];
+            };
+          };
+        };
+      }
+    );
 }
---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+input=$(</dev/stdin)
+
+<<< "$input" yq . -o=json | jv ${KRM_SCHEMA-$(dirname $0)/krm.schema.json} -
